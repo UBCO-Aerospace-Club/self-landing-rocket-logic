92ad0c94ec3341dcb6976ccc9b812d9f59bf642b
Andre C <arc.xoc@gmail.com>
2021-10-01 12:19:22 -0700

    Organization and led error codes ⚠️

---
+++ include/logging.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <Adafruit_BNO055.h>
+#include <SD.h>
+
+void dataWriteToFile(float data[7][3], File dataFile);
+void updateValues(sensors_event_t* event, float localData[3]);
+void sdLog(float[7][3]);
+void bluetoothLog();
+void errorCode(int error);
